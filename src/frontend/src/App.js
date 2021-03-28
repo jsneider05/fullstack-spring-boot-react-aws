668e26ec2a43d922d12cbb5c0cdc1831ce0a5a4d
joan.garcia <joan.garcia@ceiba.com.co>
2021-03-28 02:25:28 -0500

    DEVDEMO: SPRING-BOOT-REACT-AWS (Kick off React App, consume user API, configure proxy)

---
+++ src/frontend/src/App.js
@@ -0,0 +1,23 @@
+import { Button, Radio } from "antd";
+import { getAllStudents } from "./client";
+import "./App.css";
+
+function App() {
+  getAllStudents()
+    .then((res) => res.json())
+    .then(console.log);
+
+  return (
+    <div className="App">
+      <Button type="primary">Hello</Button>
+      <br />
+      <Radio.Group value="large">
+        <Radio.Button value="large">Large</Radio.Button>
+        <Radio.Button value="default">Default</Radio.Button>
+        <Radio.Button value="small">Small</Radio.Button>
+      </Radio.Group>
+    </div>
+  );
+}
+
+export default App;
