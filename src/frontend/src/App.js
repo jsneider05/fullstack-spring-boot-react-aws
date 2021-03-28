522ac543d9f6e468c48b89319580e0be81edfcf9
joan.garcia <joan.garcia@ceiba.com.co>
2021-03-28 12:29:13 -0500

    DEVDEMO: SPRING-BOOT-REACT-AWS (Remove some unused code)

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,4 +1,3 @@
-import { Button, Radio } from "antd";
 import { getAllStudents } from "./client";
 import "./App.css";
 
@@ -7,17 +6,7 @@ function App() {
     .then((res) => res.json())
     .then(console.log);
 
-  return (
-    <div className="App">
-      <Button type="primary">Hello</Button>
-      <br />
-      <Radio.Group value="large">
-        <Radio.Button value="large">Large</Radio.Button>
-        <Radio.Button value="default">Default</Radio.Button>
-        <Radio.Button value="small">Small</Radio.Button>
-      </Radio.Group>
-    </div>
-  );
+  return "Hello React";
 }
 
 export default App;
