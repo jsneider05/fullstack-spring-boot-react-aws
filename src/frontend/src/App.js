dabf033bbcdbc45351e0682a08a2c67fa405658a
joan.garcia <joan.garcia@ceiba.com.co>
2021-03-30 23:04:43 -0500

    DEVDEMO: SPRING-BOOT-REACT-AWS (fetch list students and store in students const using useState and useEffect. eslint prettier and styleintrc configuration)

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,31 @@
+import { useState, useEffect } from "react";
 import { getAllStudents } from "./client";
 import "./App.css";
 
 function App() {
-  getAllStudents()
-    .then((res) => res.json())
-    .then(console.log);
+  const [students, setStudents] = useState([]);
 
-  return "Hello React";
+  const fetchStudents = () =>
+    getAllStudents()
+      .then((res) => res.json())
+      .then((data) => setStudents(data));
+
+  useEffect(() => {
+    console.log("component is mounted");
+    fetchStudents();
+  }, []);
+
+  if (students.length <= 0) {
+    return "no data";
+  }
+
+  return students.map((student, index) => {
+    return (
+      <p key={index}>
+        {student.id} {student.name}
+      </p>
+    );
+  });
 }
 
 export default App;
