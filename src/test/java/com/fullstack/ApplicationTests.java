03a749e0d78073fcfb3cdff192ea04c96e23db2e
jsneider05 <joan_sneider@hotmail.com>
2021-07-07 22:03:26 -0500

    DEVDEMO: SPRING-BOOT-REACT-AWS (Test pass)

--- src/test/java/com/fullstack/ApplicationTests.java
+++ src/test/java/com/fullstack/ApplicationTests.java
@@ -1,6 +1,5 @@
 package com.fullstack;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,6 @@ class ApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Assertions.fail("Test failed");
 	}
 
 }
