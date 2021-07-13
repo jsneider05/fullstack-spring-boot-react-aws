a99dc41f0d122304f9924bd74a3d80ff527ba2e5
jsneider05 <joan_sneider@hotmail.com>
2021-07-12 20:21:03 -0500

    DEVDEMO: SPRING-BOOT-REACT-AWS (canAddUserTest)

--- src/test/java/com/fullstack/domain/service/CreateUserServiceTest.java
+++ src/test/java/com/fullstack/domain/service/CreateUserServiceTest.java
@@ -1,6 +1,8 @@
 package com.fullstack.domain.service;
 
 import static com.fullstack.domain.builder.UserBuilder.*;
+import static com.fullstack.domain.model.Gender.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 import com.fullstack.domain.model.User;
@@ -9,6 +11,7 @@ import com.fullstack.domain.port.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -29,15 +32,23 @@ class CreateUserServiceTest {
   }
 
   @Test
-  void cadAddUserSuccess() {
+  void canAddUserSuccess() {
     // Given
     User user = newUser()
+        .withName("joan")
+        .withEmail("joan@gmail.com")
+        .withGender(MALE)
         .build();
 
     // When
     underTest.addUser(user);
 
     // Then
-    verify(userDao).existsByEmail(user.getEmail());
+    ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+    verify(userRepository).save(userArgumentCaptor.capture());
+
+    User capturedUser = userArgumentCaptor.getValue();
+
+    assertThat(capturedUser).isEqualTo(user);
   }
 }
